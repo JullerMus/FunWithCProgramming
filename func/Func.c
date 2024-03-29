b4d8122825be9a3b5d2886cdfa3bcf85f2a964e2
Julius Lykkegaard <juliuslykkegaard@192-168-0-10.static.private.lan>
2024-03-29 09:29:10 +0100

    stuff

--- func/Func.c
+++ func/Func.c
@@ -3,13 +3,14 @@
 
 void swap(int, int);
 void evenOdd(int); 
-int largestInArray(int[]);
+int largestInArray(int[], int);
 
 int main(int argc, char const *argv[])
 {
 	int arr[6] = {1,3,5,1,8,7}; 
+	int alength = sizeof(arr)/sizeof(arr[0]);
 	
-	int a = largestInArray(&arr[0]);
+	int a = largestInArray(&arr[0], alength);
 	//printf("%d\n", a);
 	//printf("%d", sizeof(arr)/sizeof(arr[0]));
 
@@ -33,12 +34,12 @@ void evenOdd(int a){
 	}
 }
 
-int largestInArray(int* arr){
+int largestInArray(int* arr, int alength){
 	int largest = 0;
 	int i = 0;
 	int len = *(&arr + 1) - arr;
 	printf("%d", len);
-	while(i < 6){
+	while(i < alength){
 		if(largest < arr[i]){
 			printf("%s%d\n", "New largest found:", arr[i]);
 			largest = arr[i];
